1371a16f7aff8a4a34430c155ec2779f6a948549
Jingjing Mao <jjmao@thoughtworks.com>
2016-08-05 22:27:19 +0800

    new post verification

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,19 +4,26 @@ RSpec.feature 'post feature', type: :feature do
 
   scenario 'create new post' do
     visit '/posts'
-    sleep(5)
-    on_page_with :post_list do |page|
-        expect(page.posts_in_list.length).to eq(0)
-        sleep(1)
-        page.new_post_link.click
-        sleep(1)
-        page.perform :fill_post, 'my first post', 'I wanna be the full-stack dev'
-        sleep(1)
-        page.create_post_button.click
-        sleep(1)
-        expect(page.notice_message).to eq('Post was successfully created.')
-        page.back_link.click
-        expect(page.posts_in_list.length).to eq(1)
-    end
+    sleep(1)
+    # expect(page).not_to have_content('initialize')
+    # expect(page).to have_content('start loading...')
+    # expect(page).to have_content('finish loading')
+    click_link('New Post')
+    fill_in('post_title', with: 'my first post')
+    sleep(1)
+    fill_in('post_content', with: 'I wanna be the full-stack dev')
+    sleep(1)
+    click_button('Create Post')
+    sleep(1)
+    expect(page).to have_content('Post was successfully created.')
+    click_link('Back')
+    # visit '/posts'
+    sleep(1)
+    expect(page).to have_css("table td", text: "my first post")
+    expect(page).not_to have_content('initialize component')
+    expect(page).to have_content('start loading...')
+    # expect(page).to have_content('finish loading')
+    sleep(2)
+    expect(page).to have_css("ul li", text: "my first post")
   end
 end
