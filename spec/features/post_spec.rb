0d7d2e0dd4353d05cb493e3ed605722407277e2e
Jingjing Mao <jjmao@thoughtworks.com>
2016-08-19 19:16:55 +0800

    modify post_spec to use find instead of sleep and use meaningful business selector

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,26 +4,25 @@ RSpec.feature 'post feature', type: :feature do
 
   scenario 'create new post' do
     visit '/posts'
-    sleep(1)
-    # expect(page).not_to have_content('initialize')
-    # expect(page).to have_content('start loading...')
-    # expect(page).to have_content('finish loading')
+
+    find('a[href="/posts/new"]')
     click_link('New Post')
+
+    find('#post_title')
     fill_in('post_title', with: 'my first post')
-    sleep(1)
+    find('#post_content')
     fill_in('post_content', with: 'I wanna be the full-stack dev')
-    sleep(1)
+    find('input[name=commit]')
     click_button('Create Post')
-    sleep(1)
     expect(page).to have_content('Post was successfully created.')
+
     click_link('Back')
-    # visit '/posts'
-    sleep(1)
+
+    find('.tips')
     expect(page).to have_css("table td", text: "my first post")
     expect(page).not_to have_content('initialize component')
     expect(page).to have_content('start loading...')
-    # expect(page).to have_content('finish loading')
-    sleep(2)
-    expect(page).to have_css("ul li", text: "my first post")
+    find('.top-10-list')
+    expect(page).to have_css(".top-10-list", text: "my first post")
   end
 end
